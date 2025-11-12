b37d0c14775648a6273b03e8ed749dc7246a91bb
FARIS <salmanfaris3542@gmail.com>
2025-11-12 16:01:31 +0530

    new

---
+++ vowel.py
@@ -0,0 +1,9 @@
+word=input("Enter a word:")
+vowels=['a','e','i','o','u','A','E','I','O','U']
+vowel_list=[]
+for ch in word:
+    if ch in vowels:
+         vowel_list.append(ch)
+print("vowels:", vowel_list)
+
+
